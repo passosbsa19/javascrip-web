cf77418a2c7149b59083ad6e6ac0f3f0d0154c56
Samara Passos <105375430+passosbsa19@users.noreply.github.com>
2022-11-17 08:04:52 -0300

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -2,6 +2,7 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista;
 
 var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso=paciente.querySelector(".info-peso");
 
-
-console.log(paciente);
+console.log(paciente); // tr
+console.log(tdPeso); // td que tem o peso
