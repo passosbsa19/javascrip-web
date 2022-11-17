1b4ff7ae80480343921d6f868451eed5f9f00671
Samara Passos <105375430+passosbsa19@users.noreply.github.com>
2022-11-17 08:18:35 -0300

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -4,5 +4,9 @@ titulo.textContent = "Aparecida Nutricionista;
 var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso=paciente.querySelector(".info-peso");
 
-console.log(paciente); // tr
-console.log(tdPeso); // td que tem o peso
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / altura * altura; // 100 / 2.0 * 2.0 = 100 / 4 = 25
